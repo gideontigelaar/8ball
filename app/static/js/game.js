84133a5d952f937f546ae67f8e5dde869ba959e4
KoningWoefie <sander@hdms.nl>
2026-02-19 22:31:16 +0100

    P5js base
    Game code for canvas scalable with aspect ratio. Adding p5js and using that to make simple canvas and circle

---
+++ app/static/js/game.js
@@ -0,0 +1,58 @@
+let cnv;
+let aspectRatio = 16/9;
+let oldWidth, oldHeight
+const margin = convertRemToPixels(8);
+
+function setup() 
+{
+    cnv = createCanvas((windowHeight - margin) * aspectRatio, windowHeight - margin);
+    cnv.position((windowWidth - width)/2,(windowHeight - height)/2)
+    oldWidth = windowWidth
+    oldHeight = windowHeight
+}
+function draw() 
+{
+    background(220);
+    //when mouse button is pressed, circles turn black
+    if (mouseIsPressed === true) 
+    {
+        fill(0);
+    } 
+    else 
+    {
+        fill(255);
+    }
+
+    //white circles drawn at mouse position
+    circle(mouseX, mouseY, 100);
+}
+
+function windowResized() 
+{
+    let availableWidth = windowWidth - margin;
+    let availableHeight = windowHeight - margin;
+
+    let canvasWidth = availableWidth;
+    let canvasHeight = canvasWidth / aspectRatio;
+
+    // If height is too big, fit by height instead
+    if (canvasHeight > availableHeight) 
+    {
+        canvasHeight = availableHeight;
+        canvasWidth = canvasHeight * aspectRatio;
+    }
+
+    resizeCanvas(canvasWidth, canvasHeight);
+    cnv.position((windowWidth - width)/2,(windowHeight - height)/2)
+
+    oldWidth = newSizeX;
+    oldHeight = newSizeY;
+}
+
+// Source - https://stackoverflow.com/a/42769683
+// Posted by etham
+// Retrieved 2026-02-19, License - CC BY-SA 3.0
+
+function convertRemToPixels(rem) {    
+    return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
